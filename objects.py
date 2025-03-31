aa01394a5c0c61593109e34c5027fa896b1739e2
matej.stoklasek <matej.stoklasek@orbiszlin.cz>
2025-03-31 14:28:38 +0200

    Úkol

--- objects.py
+++ objects.py
@@ -1,11 +1,16 @@
-dog_weight = 50
-dog_name = "Vlčák"
+#dog_weight = 50
+#dog_name = "Vlčák"
 
-class Dog:
-    name = "Vlčák"
-    weight = 50
+class Dog: #Přepis objektu
+    # name = "Vlčák"
+    # weight = 50
 
-print(f"Jméno: {dog_name} a váha: {dog_weight}kg")
+    def __init__(self): #print self odkazuje na třídu Dog /
+        self.name = "Vlčák"
+        self.weight = 25
 
-dog = Dog()
+
+#print(f"Jméno: {dog_name} a váha: {dog_weight}kg")
+
+dog = Dog() # vždy první proměná malým. 2. Objekt každé slovo velkým / Istance třidy = objekt / v ojektu má vlastnosti
 print(f"Jméno: {dog.name} a váha: {dog.weight}kg")
